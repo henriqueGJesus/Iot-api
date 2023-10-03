9f22793c93feea1102587c238b917efd47969462
Otavio-Miguel-Rocha <otaviorochajcm@gmail.com>
2023-10-03 18:26:59 -0300

    Implementa websocket no index.js e segmenta em casa

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.get('/api/casa/:id/portao/:acao', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
+    console.log(`Servidor rodando na porta ${port} (API)` );
 });
 
 /*
@@ -83,5 +83,44 @@ app.listen(port, () => {
     Utilizar a biblioteca mqtt.js no frontend
     para permitir a conexão com o broker MQTT.
 */
+
+/* WEBSOCKET ======================================== */
+const WebSocket = require('ws');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('WebSocket server running.');
+});
+
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+    console.log('WebSocket connection established.');
+
+    // Handle WebSocket messages here.
+    ws.on('message', (message) => {
+        const msg = parseInt(message);
+        console.log("Casa conectada: " + msg);
+
+        if(msg == 1){
+            //CASA 1
+            ws.send(JSON.stringify({ message: '1' }));    
+
+        } else if(msg == 2){
+            //CASA 2
+            ws.send(JSON.stringify({ message: '2' }));    
+
+        } else if(msg == 3){
+            //CASA 3
+            ws.send(JSON.stringify({ message: '3' }));    
+        }
+    });
+});
+
+server.listen(433, () => {
+    console.log(`Server is listening on port 433 (Websocket)`);
+});
+
 
 
