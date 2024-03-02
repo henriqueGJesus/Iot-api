aa6c43f37a64e7ecc799f344b5f5969251bce384
henriqueGjesus <hj30032006@gmail.com>
2024-03-01 21:56:05 -0300

    api-get-nãofunciona

--- index.js
+++ index.js
@@ -5,15 +5,17 @@ const mqtt = require('mqtt');
 const connectUrl = `mqtt://mqtt.tago.io:1883`;
 const { mqtt_especificacoes } = require('./src/mqtt/mqtt');
 const client = mqtt.connect(connectUrl, mqtt_especificacoes());
-const topic = "/fazenda";
+const topic = "fazenda";
+const topic2="temperatura"
 
 // Se inscrevendo no tópico
-client.subscribe(topic, (err) => {
-    if (err) {
-        console.error("Erro ao se inscrever no tópico:", err);
-    } else {
-        console.log("Inscrição no tópico bem-sucedida!");
-        
+client.subscribe(topic);
+client.subscribe();
+
+client.on("message", (receivedTopic, message) => {
+    if (receivedTopic === topic2) {
+        console.log("Mensagem recebida no tópico 'dadoscoleta':", message.toString());
+        // Faça o que for necessário com a mensagem recebida
     }
 });
 
@@ -31,9 +33,9 @@ app.use(express.json());
 
 
 // app.post(`${topic}:id/luz`, (req, res) => luz(req, res, client));
-app.get(`/${topic}/temperatura`, () => temperatura());
+app.get(`/${topic}/temperatura`, (req,res) => temperatura(req,res));
 
-app.post(`${topic}`, (req, res) => temperaturaPostarTopico(req, res,client));
+app.post(`/${topic}`, (req, res) => temperaturaPostarTopico(req, res,client));
 
 app.listen(3000, () => console.log(`API rodando na porta ${3000}`));
 
--- src/requisições/temperatura.js
+++ src/requisições/temperatura.js
@@ -1,24 +1,24 @@
-const topic= "fazenda"
+const topic = "dadoscoleta"
 
 const mqtt = require('mqtt');
 const connectUrl = `mqtt://broker.mqttdashboard.com`;
 const { mqtt_especificacoes2 } = require('../mqtt/mqttSetor2');
 const clientJonatas = mqtt.connect(connectUrl, mqtt_especificacoes2());
 const topicJonatas = "setor1-setor2";
+let temperaturaParaMandarFront= [];
 
-function temperatura(res, temperatura) {
-    if (temperatura < 12 || temperatura > 28) {
-        return res.status(200).json({ gas: 23 });
-    } else {
-        return res.status(200).json({ gas: temperatura });   
-    }
+function temperatura(req,res) {
+    console.log(temperaturaParaMandarFront)
+        return res.status(200).json(temperaturaParaMandarFront);
 }
 
-function temperaturaPostarTopico(req, res, client){
+function temperaturaPostarTopico(req, res, client) {
+
     const mensagemJSON = JSON.stringify(req.body);
-   
+
+
     // publicando o objeto recebido no nosso topico
-    client.publish(topic, mensagemJSON, (err) => {
+    client.publish("dadoscoleta", JSON.stringify(mensagemJSON), (err) => {
         if (err) {
             console.error('Erro ao publicar mensagem no MQTT:', err);
             return res.status(500).send('Erro interno ao enviar mensagem MQTT');
@@ -26,17 +26,27 @@ function temperaturaPostarTopico(req, res, client){
 
         console.log('Mensagem publicada com sucesso no MQTT');
         // Retornar um status 200 OK
-        
+
     });
-    publicarObjetoTopicoSetor2(JSON.stringify(req.body.value),res)    
+    const topic2 = "temperautra"
+   
+    client.on("message", (receivedTopic, message) => {
+        if (receivedTopic === topic2) {
+            temperaturaParaMandarFront.push(message)
+            console.log(temperaturaParaMandarFront)
+            // Faça o que for necessário com a mensagem recebida
+        }
+    });
+   
+    publicarObjetoTopicoSetor2(JSON.stringify(req.body.value), res)
 }
 
-function publicarObjetoTopicoSetor2(obejtoEnvioSetor2,res){
+function publicarObjetoTopicoSetor2(obejtoEnvioSetor2, res) {
     console.log(obejtoEnvioSetor2)
     const mensagemSetor2 = obejtoEnvioSetor2
 
-    clientJonatas.publish(topicJonatas, mensagemSetor2, (err)=>{
-        if(err){
+    clientJonatas.publish(topicJonatas, mensagemSetor2, (err) => {
+        if (err) {
             console.log(err)
             return res.status(500).send('erro ao enviar mensagem jonatas')
 
